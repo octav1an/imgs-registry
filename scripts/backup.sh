e4b5c8d2627dcd30be3c59a40ab8eb46c796541d
octav1an <octav1an@hotmail.com>
2024-12-24 20:51:36 +0100

    update backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,7 @@
 VOLUME_NAME=local-registry_data
 
 ACTION=$1
+BACKUP_NAME=$2
 
 if [ -z $ACTION ]; then
   echo "backing up registry..."
@@ -10,7 +11,7 @@ if [ -z $ACTION ]; then
       -v "$VOLUME_NAME":/backup-volume \
       -v "$(pwd)":/backup \
       busybox \
-      tar -zcvf /backup/my-backup.tar.gz /backup-volume
+      tar -zcvf /backup/backup-$(date +%Y-%m-%d_%H-%M-%S).tar.gz /backup-volume
 fi
 
 if [ "$ACTION" == "restore" ]; then
@@ -19,7 +20,7 @@ if [ "$ACTION" == "restore" ]; then
       -v "$VOLUME_NAME":/backup-volume \
       -v "$(pwd)":/backup \
       busybox \
-      tar -xzvf /backup/my-backup.tar.gz --strip-components 1 -C /backup-volume
+      tar -xzvf /backup/$BACKUP_NAME --strip-components 1 -C /backup-volume
 else
   echo "action not allowed"
 fi
